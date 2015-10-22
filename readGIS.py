f0e41968790daed2ce3e1ca8fe4c56b30c2746a8
krisvanneste <kris.vanneste@oma.be>
2015-10-22 19:02:00 +0000

    Added out_srs and encoding parameters to read_GIS_file function.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/geo/trunk@3361 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- readGIS.py
+++ readGIS.py
@@ -1,9 +1,11 @@
 import os
+
 import ogr, osr
 
+from mapping.geo.coordtrans import wgs84
 
 
-def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
+def read_GIS_file(GIS_filespec, layer_num=0, out_srs=wgs84, encoding="guess", verbose=True):
 	"""
 	Read GIS file.
 
@@ -11,6 +13,14 @@ def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
 		String, full path to GIS file to read
 	:param layer_num:
 		Integer, index of layer to read (default: 0)
+	:param out_srs:
+		instance of :class:`ogr.SpatialReference`
+		spatial reference system into which coordinates will be transformed
+		If None, native spatial reference system will be used
+		(default: wgs84)
+	:param encoding:
+		str, unicode encoding
+		(default: "guess", will try to guess, but this may fail)
 	:param verbose:
 		Boolean, whether or not to print information while reading
 		GIS table (default: True)
@@ -19,15 +29,11 @@ def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
 		list of dictionaries corresponding to GIS records.
 		Each record contains the following keys:
 		- 'obj': corresponding value is instance of :class:`osgeo.ogr.Geometry`
-			with all coordinates in WGS84 reference system
+			with all coordinates in spatial reference system in :param:`out_srs`
 		- keys corresponding to data attribute names
+		Note that keys and string values are decoded into unicode
+		if :param:`encoding' is not set
 	"""
-	## Construct WGS84 projection system
-	from mapping.geo.coordtrans import lambert1972
-
-	wgs84 = osr.SpatialReference()
-	wgs84.SetWellKnownGeogCS("WGS84")
-
 	ds = ogr.Open(GIS_filespec, 0)
 	if ds is None:
 		raise Exception("OGR failed to open %s" % GIS_filespec)
@@ -39,6 +45,19 @@ def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
 	else:
 		raise Exception("File contains less than %d layers!" % layer_num)
 
+	## Try to guess encoding
+	## See: https://nelsonslog.wordpress.com/2015/01/15/ogrpython-vs-unicode/
+	## and https://github.com/openaddresses/machine/blob/e5099e5a23b8ab6571227c5f8487034c8a8b7cc2/openaddr/conform.py#L213
+	# TODO: isn't there a more robust way?
+	if encoding == "guess":
+		from locale import getpreferredencoding
+		ogr_recoding = layer.TestCapability(ogr.OLCStringsAsUTF8)
+		is_shapefile = ds.GetDriver().GetName() == 'ESRI Shapefile'
+
+		encoding = ((ogr_recoding and 'UTF-8')
+			or (is_shapefile and 'ISO-8859-1')
+			or getpreferredencoding())
+
 	## Get all field names
 	field_names = []
 	ld = layer.GetLayerDefn()
@@ -47,17 +66,23 @@ def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
 		field_names.append(fd.GetName())
 
 	## Set up transformation between table coordsys and wgs84
-	tab_sr = layer.GetSpatialRef()
+	tab_srs = layer.GetSpatialRef()
 
 	## Hack to fix earlier MapInfo implementation of Lambert1972
-	if os.path.splitext(GIS_filespec)[-1].upper() == ".TAB" and 'DATUM["Belgium_Hayford"' in tab_sr.ExportToWkt():
-		if tab_sr.IsProjected():
+	if os.path.splitext(GIS_filespec)[-1].upper() == ".TAB" and 'DATUM["Belgium_Hayford"' in tab_srs.ExportToWkt():
+		from mapping.geo.coordtrans import lambert1972
+		if tab_srs.IsProjected():
 			print("Fixing older MapInfo implementation of Lambert1972...")
-			tab_sr = lambert1972
+			tab_srs = lambert1972
 		else:
+			# TODO
 			print("Warning: older MapInfo implementation of Lambert1972, coordinates may be shifted!")
-		#tab_sr.CopyGeogCSFrom(lambert1972)
-	coordTrans = osr.CoordinateTransformation(tab_sr, wgs84)
+		#tab_srs.CopyGeogCSFrom(lambert1972)
+
+	if out_srs:
+		coordTrans = osr.CoordinateTransformation(tab_srs, out_srs)
+	else:
+		coordTrans = None
 
 	## Loop over features in layer
 	num_features = layer.GetFeatureCount()
@@ -75,15 +100,21 @@ def read_GIS_file(GIS_filespec, layer_num=0, verbose=True):
 			## otherwise python will crash
 			## See http://trac.osgeo.org/gdal/wiki/PythonGotchas
 			geom = feature.GetGeometryRef().Clone()
-			geom.AssignSpatialReference(tab_sr)
-			geom.Transform(coordTrans)
+			geom.AssignSpatialReference(tab_srs)
+			if coordTrans:
+				geom.Transform(coordTrans)
 			#geom.CloseRings()
 			feature_data['obj'] = geom
 
 			## Get feature attributes
-			for field_name in field_names:
-				feature_data[field_name] = feature.GetField(field_name)
-				# TODO: convert strings to unicode based on charset in TAB definition
+			for i, field_name in enumerate(field_names):
+				value = feature.GetField(i)
+				## Convert field names and string values to unicode
+				if encoding:
+					field_name = field_name.decode(encoding)
+					if isinstance(value, str):
+						value = value.decode(encoding)
+				feature_data[field_name] = value
 			records.append(feature_data)
 	return records
 
