41fb9244ceea1774dbec9479497880131b8e64ce
krisvanneste <kris.vanneste@oma.be>
2016-10-02 18:46:14 +0000

    Improved fix_mi_lambert.py script.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/geo/trunk@3938 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- fix_mi_lambert.py
+++ fix_mi_lambert.py
@@ -5,83 +5,119 @@ import osr, ogr, gdal
 gdal.UseExceptions()
 
 
-def fix_mi_lambert_ogr(gis_filespec):
+def backup_mi_file(mi_filespec, backup_filespec="", overwrite=False):
+	extensions = [".TAB", ".DAT", ".MAP", ".ID", ".IND"]
+	folder, filename = os.path.split(mi_filespec)
+	basename, _ = os.path.splitext(filename)
+	if backup_filespec:
+		backup_folder, backup_filename = os.path.split(backup_filespec)
+		backup_basename, _ = os.path.splitext(backup_filename)
+	else:
+		backup_folder = folder
+		backup_basename = "Copy of " + basename
+	for ext in extensions:
+		filespec = os.path.join(folder, basename + ext)
+		backup_filespec = os.path.join(backup_folder, backup_basename + ext)
+		if os.path.exists(filespec):
+			if os.path.exists(backup_filespec):
+				if overwrite:
+					os.unlink(backup_filespec)
+				else:
+					print("Warning: not overwriting backup file %s!" % backup_filespec)
+					return
+			os.rename(filespec, backup_filespec)
+
+
+def fix_mi_lambert_ogr(mi_filespec, overwrite_backup=False):
 	from mapping.geo.coordtrans import lambert1972
 
-	in_ds = ogr.Open(gis_filespec, 0)
+	in_ds = ogr.Open(mi_filespec, 0)
+
+	## Non-vector MapInfo files cannot be read by ogr, try with MIPython
+	if in_ds is None and os.path.exists(mi_filespec):
+		return fix_mi_lambert(mi_filespec, overwrite_backup=overwrite_backup)
+
 	in_layer = in_ds.GetLayer(0)
 	in_srs = in_layer.GetSpatialRef()
 	if 'DATUM["Belgium_Hayford"' in in_srs.ExportToWkt():
 		if in_srs.IsProjected():
-			print("Fixing %s..." % gis_filespec)
+			print("Fixing %s... with ogr/gdal" % mi_filespec)
 			#in_srs.CopyGeogCSFrom(lambert1972)
-			#layer.SyncToDisk()
 
-			# Create the output Layer
-			folder, filename = os.path.split(gis_filespec)
+			folder, filename = os.path.split(mi_filespec)
 			basename, ext = os.path.splitext(filename)
+
+			## Create backup first
 			backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
-			out_filespec = os.path.join(folder, "Copy of " + filename)
+			print("Copying backup file %s" % backup_filespec)
+			in_ds.Destroy()
+			backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
+			in_ds = ogr.Open(backup_filespec, 0)
+			in_layer = in_ds.GetLayer(0)
+
+			## Create the output tab file
 			driver = ogr.GetDriverByName("MapInfo File")
+			out_ds = driver.CreateDataSource(mi_filespec)
 
-			# Remove output tab file if it already exists
-			if os.path.exists(out_filespec):
-				driver.DeleteDataSource(out_filespec)
+			## Create the output Layer
+			## If input geometry type is wkbUnknown, output features are not
+			## created, so we just set linestring, but it doesn't really matter
+			geom_type = in_layer.GetGeomType() or ogr.wkbLineString
+			out_layer = out_ds.CreateLayer(in_layer.GetName(), srs=lambert1972,
+											geom_type=geom_type)
 
-			# Create the output tab file
-			out_ds = driver.CreateDataSource(out_filespec)
-			out_lyr_name = os.path.splitext(filename)[0]
-			out_layer = out_ds.CreateLayer(out_lyr_name, srs=lambert1972, geom_type=in_layer.GetGeomType())
-
-			# Copy input Layer Fields to the output Layer
+			## Copy input Layer Fields to the output Layer
 			in_layer_defn = in_layer.GetLayerDefn()
-			for i in range(0, in_layer_defn.GetFieldCount()):
+			for i in range(in_layer_defn.GetFieldCount()):
 				field_defn = in_layer_defn.GetFieldDefn(i)
 				out_layer.CreateField(field_defn)
 
-			# Get the output Layer's Feature Definition
+			## Get the output Layer's Feature Definition
 			out_layer_defn = out_layer.GetLayerDefn()
 
-			# Add features to the ouput Layer
+			## Add features to the ouput Layer
 			for in_feature in in_layer:
-				# Create output Feature
+				## Create output Feature
 				out_feature = ogr.Feature(out_layer_defn)
 
-				# Add field values from input Layer
-				for i in range(0, out_layer_defn.GetFieldCount()):
+				## Add field values from input Layer
+				for i in range(out_layer_defn.GetFieldCount()):
 					field_defn = out_layer_defn.GetFieldDefn(i)
 					out_feature.SetField(out_layer_defn.GetFieldDefn(i).GetNameRef(),
 						in_feature.GetField(i))
 
-				# Set geometry
+				## Set geometry if present
 				geom = in_feature.GetGeometryRef()
-				out_feature.SetGeometry(geom.Clone())
+				if geom:
+					wkt = geom.ExportToWkt()
+					out_feature.SetGeometry(geom.Clone())
+				#out_geom = ogr.CreateGeometryFromWkt(wkt)
+				#out_feature.SetGeometry(out_geom)
 
-				# Add new feature to output Layer
-				out_layer.CreateFeature(out_feature)
+				## Add new feature to output Layer
+				errcode = out_layer.CreateFeature(out_feature)
+				#print errcode
 
-				# Destroy the feature to free resources
+				## Destroy the feature to free resources
 				out_feature.Destroy()
 
+			out_layer.SyncToDisk()
+
 			# Close DataSources
-			driver.CopyDataSource(in_ds, backup_filespec)
 			in_ds.Destroy()
-			driver.DeleteDataSource(gis_filespec)
-			driver.CopyDataSource(out_ds, gis_filespec)
 			out_ds.Destroy()
-			driver.DeleteDataSource(out_filespec)
 
 		else:
 			# TODO
-			print("Warning: older MapInfo implementation of Lambert1972, coordinates may be shifted!")
+			print("Warning: %s has older MapInfo implementation of Lambert1972, not fixed!" % mi_filespec)
 
 
 
-def fix_mi_lambert(gis_filespec, remove_backup=False):
+def fix_mi_lambert(mi_filespec, overwrite_backup=False):
 	import mapping.MIPython as MI
 
 	app = MI.Application()
-	tab = app.OpenTable(gis_filespec)
+	tab = app.OpenTable(mi_filespec)
 	coordsys = tab.GetCoordsys()
 	bounds = coordsys.GetBounds()
 	coordsys.DropBounds()
@@ -90,41 +126,46 @@ def fix_mi_lambert(gis_filespec, remove_backup=False):
 	new_lambert_clause = 'CoordSys Earth Projection 3, 1019, "m", 4.3674866667, 90.0, 49.8333339, 51.1666672333, 150000.013, 5400088.438'
 
 	if coordsys.Clause() == old_lambert_clause:
-		print("Fixing %s..." % gis_filespec)
-		coordsys.FromClause(new_lambert_clause)
-		coordsys.SetBounds(bounds)
-		tab.SetCoordsys(coordsys)
+		print("Fixing %s... with MIPython" % mi_filespec)
+
+		## Create backup first
+		tab.Close()
+		folder, filename = os.path.split(mi_filespec)
+		basename, ext = os.path.splitext(filename)
+		backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
+		print("Copying backup file %s" % backup_filespec)
+		backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
+		tab = app.OpenTable(backup_filespec)
+		tab.Backup(mi_filespec)
 		tab.Close()
 
-		try:
-			tab = app.OpenTable(gis_filespec)
-		except:
-			raise
-		else:
-			folder, filename = os.path.split(gis_filespec)
-			backup_filespec = os.path.join(folder, "Copy of " + filename)
-			if remove_backup and os.path.exists(backup_filespec):
-				print("Removing backup copy...")
-				backup_tab = app.OpenTable(backup_filespec)
-				backup_tab.Drop()
-			tab.Close()
+		## Fix coordinate system
+		tab = app.OpenTable(mi_filespec)
+		coordsys.FromClause(new_lambert_clause)
+		coordsys.SetBounds(bounds)
+		tab.SetCoordsys(coordsys, backup=False)
+		tab.Close()
 
 	else:
-		print coordsys.Clause()
+		tab.Close()
 
 
-def fix_mi_lambert_folder(folder, remove_backup=False):
+def fix_mi_lambert_folder_ogr(folder, overwrite_backup=False):
 	for dirpath, dirnames, filenames in os.walk(folder):
 		for filename in filenames:
-			if os.path.splitext(filename)[-1].upper() == ".TAB":
+			if (os.path.splitext(filename)[-1].upper() == ".TAB"
+				and not "old lambert72" in filename):
 				mi_filespec = os.path.join(dirpath, filename)
-				fix_mi_lambert(mi_filespec, remove_backup=remove_backup)
+				fix_mi_lambert_ogr(mi_filespec, overwrite_backup=overwrite_backup)
+
 
 
 if __name__ == "__main__":
-	#gis_filespec = r"C:\Temp\MI\QUAT_18_TERRASGRENS.tab"
-	#gis_filespec = r"C:\Temp\MI\Quaternary\BasisQ_18.TAB"
-	#fix_mi_lambert(gis_filespec)
+	mi_filespec = r"C:\Temp\MI\QUAT_18_BREUK.tab"
+	#mi_filespec = r"C:\Temp\MI\Quaternary\BasisQ_18.TAB"
+	#backup_mi_file(mi_filespec, r"C:\Temp\MI\QUAT_18_BREUK (old lambert72).tab", overwrite=True)
+
+	#fix_mi_lambert_ogr(mi_filespec)
 	folder = r"C:\Temp\MI\Quaternary"
-	fix_mi_lambert_folder(folder)
+	fix_mi_lambert_folder_ogr(folder)
 
