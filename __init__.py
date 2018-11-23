6836a29c31d7b292e75332d9babdc88e93fccac3
krisvanneste <kris.vanneste@oma.be>
2018-11-23 15:54:42 +0000

    Added short description of geotools package in __init__.py.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/geotools/trunk@4790 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
-#
-# Empty file necessary for python to recognise directory as package
-#
+"""
+geotools: Container package for various geospatial tools.
+
+Contains following submodules:
+- angle: some numeric operations on geographic angles or azimuths
+- coordtrans: coordinate transformation between different spatial reference systems
+- geodetic: distance/azimuth calculations in cartesian and spherical coordsys
+- geotiff: write single-band and multi-band GeoTiffs
+- read_gis: read GIS files in various formats
+
+Note that these submodules are not loaded in the package's namespace,
+but must be imported separately.
+
+Author: Kris Vanneste
+"""
