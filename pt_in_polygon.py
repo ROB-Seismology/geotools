1fbe8e365468d517d2786a55e2c53476b0359b90
krisvanneste <kris.vanneste@oma.be>
2020-05-03 14:33:02 +0000

    Renamed filter_collection_by_polygon function to filter_points_by_polygon, replaced 'pt_collection' argument with 'longitudes' and 'latitudes' arguments, removed 'as_indexes' argument, and only return indexes.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/geotools/trunk@6634 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- pt_in_polygon.py
+++ pt_in_polygon.py
@@ -8,30 +8,25 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from osgeo import ogr
 
 
-__all__ = ['filter_collection_by_polygon']
+__all__ = ['filter_points_by_polygon']
 
 
-def filter_collection_by_polygon(pt_collection, poly_obj, as_indexes=False):
+def filter_points_by_polygon(longitudes, latitudes, poly_obj):
 	"""
 	Subselect points from a point collection that are situated
 	inside a polygon
 
-	:param pt_collection:
-		instance of a class supporting the following:
-		- iteration, indexing, giving objects having 'lon' and 'lat'
-		  properties;
-		- get_longitudes and get_latitudes methods;
+	:param longitudes:
+		array-like, longitudes (in degrees)
+	:param latitudes:
+		array-like, latitudes (in degrees)
 	:param poly_obj:
 		polygon or closed linestring object (ogr geometry object
 		or oqhazlib.geo.polygon.Polygon object)
-	:param as_indexes:
-		bool, whether to return indexes instead of collection objects
-		(default: False)
 
 	:return:
 		(points_inside, points_outside) tuple
-		lists with instances of objects that are part of the collection
-		(or indexes) that are inside/outside the polygon
+		lists with indexes of points that are inside/outside the polygon
 	"""
 	points_inside, points_outside = [], []
 
@@ -62,17 +57,12 @@ def filter_collection_by_polygon(pt_collection, poly_obj, as_indexes=False):
 					poly_obj = ogr.CreateGeometryFromWkt(wkt)
 				else:
 					return None
-			filtered_points = []
-			for idx, item in enumerate(pt_collection):
-				point.SetPoint(0, item.lon, item.lat)
+			for idx, (lon, lat) in enumerate(zip(longitudes, latitudes)):
+				point.SetPoint(0, lon, lat)
 				if point.Within(poly_obj):
-					point_list = points_inside
+					points_inside.append(idx)
 				else:
-					point_list = points_outside
-				if as_indexes:
-					point_list.append(idx)
-				else:
-					point_list.append(item)
+					points_outside.append(idx)
 
 		else:
 			msg = 'Warning: %s not a polygon geometry!'
@@ -82,19 +72,14 @@ def filter_collection_by_polygon(pt_collection, poly_obj, as_indexes=False):
 	else:
 		import openquake.hazardlib as oqhazlib
 		if isinstance(poly_obj, oqhazlib.geo.Polygon):
-			mesh = oqhazlib.geo.Mesh(pt_collection.get_longitudes(),
-									pt_collection.get_latitudes(), depths=None)
+			mesh = oqhazlib.geo.Mesh(longitudes, latitudes, depths=None)
 			intersects = poly_obj.intersects(mesh)
 			in_polygon = (intersects == True)
 			for idx in range(len(in_polygon)):
 				if in_polygon[idx]:
-					point_list = points_inside
-				else:
-					point_list = points_outside
-				if as_indexes:
 					points_inside.append(idx)
 				else:
-					points_inside.append(pt_collection[idx])
+					points_outside.append(idx)
 		else:
 			raise Exception("poly_obj not recognized!")
 
