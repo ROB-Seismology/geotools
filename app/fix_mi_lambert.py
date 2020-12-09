8b5a271e2307ef170a1eac056d26c120b08cce88
krisvanneste <kris.vanneste@oma.be>
2020-12-09 14:31:26 +0000

    Improved fix_mi_lambert_folder_ogr function, and added 'encoding' and 'dry_run' arguments. Renamed fix_mi_lambert function to fix_mi_lambert_mi and added 'dry_run' argument. Renamed fix_mi_lambert_folder_ogr function to fix_mi_lambert_folder and added 'method', 'encoding', 'dry_run' and 'recursive' arguments.
    git-svn-id: https://svn.seismo.oma.be/svn/seismo/mapping/geotools/trunk@7257 40b490c5-b4d9-47cb-8714-9bef99b524d5

--- app/fix_mi_lambert.py
+++ app/fix_mi_lambert.py
@@ -3,13 +3,20 @@ Fix imprecise implementation of Lambert1972 coordinate system
 in older MapInfo files
 """
 
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function
 
 
 import os
-import osr, ogr, gdal
+os.environ["PYTHONIOENCODING"] = "latin-1"
 
-gdal.UseExceptions()
+import sys
+if sys.version_info[0] == 2:
+	PY2 = True
+else:
+	PY2 = False
+
+import osr, ogr
+osr.UseExceptions()
 
 
 def backup_mi_file(mi_filespec, backup_filespec="", overwrite=False):
@@ -36,95 +43,138 @@ def backup_mi_file(mi_filespec, backup_filespec="", overwrite=False):
 			os.rename(filespec, backup_filespec)
 
 
-def fix_mi_lambert_ogr(mi_filespec, overwrite_backup=False):
+def fix_mi_lambert_ogr(mi_filespec, overwrite_backup=False, encoding='latin-1',
+							dry_run=False):
+	"""
+	Fix Belgian Lambert 1972 projection in MapInfo files
+
+	:param mi_filespec:
+		str, full path to MapInfo TAB file
+	:param overwrite_backup:
+		bool, whether or not to overwrite previous backup files (with appendix
+		'(old lambert72)')
+		(default: False)
+	:param encoding:
+		str, character encoding, required for PY3, which messes up the encoding
+		(default: 'latin-1')
+	:param dry_run:
+		bool, whether or not to keep files untouched, only reporting which ones
+		have a faulty projection
+		(default: False)
+	"""
 	from mapping.geotools.coordtrans import LAMBERT1972
 
 	in_ds = ogr.Open(mi_filespec, 0)
 
 	## Non-vector MapInfo files cannot be read by ogr, try with MIPython
 	if in_ds is None and os.path.exists(mi_filespec):
-		return fix_mi_lambert(mi_filespec, overwrite_backup=overwrite_backup)
+		print('%s: Not a native MapInfo TAB file' % os.path.split(mi_filespec)[-1])
+		#return fix_mi_lambert(mi_filespec, overwrite_backup=overwrite_backup)
+		return
 
 	in_layer = in_ds.GetLayer(0)
 	in_srs = in_layer.GetSpatialRef()
 	wkt = in_srs.ExportToWkt()
 	if ('DATUM["Belgium_Hayford' in wkt or 'DATUM["MIF 999' in wkt
 		or 'TOWGS84[0,0,0,0,0,0,0]' in wkt):
+		print('%s: detected older MapInfo implementation of Lambert1972'
+				% os.path.split(mi_filespec)[-1])
 		if in_srs.IsProjected():
-			print("Fixing %s... with ogr/gdal" % mi_filespec)
-			#in_srs.CopyGeogCSFrom(lambert1972)
+			out_srs = LAMBERT1972
+		else:
+			## Doesn't seem to work...
+			out_srs = LAMBERT1972.CloneGeogCS()
+			print("Warning: unprojected lon/lat not supported, not fixed!")
+			return
 
-			folder, filename = os.path.split(mi_filespec)
-			basename, ext = os.path.splitext(filename)
+		if dry_run:
+			return
 
-			## Create backup first
-			backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
-			print("Copying backup file %s" % backup_filespec)
-			in_ds.Destroy()
-			backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
-			in_ds = ogr.Open(backup_filespec, 0)
-			in_layer = in_ds.GetLayer(0)
+		print("Fixing projection with ogr/gdal")
+		#in_srs.CopyGeogCSFrom(lambert1972)
 
-			## Create the output tab file
-			driver = ogr.GetDriverByName("MapInfo File")
-			out_ds = driver.CreateDataSource(mi_filespec)
+		folder, filename = os.path.split(mi_filespec)
+		basename, ext = os.path.splitext(filename)
 
-			## Create the output Layer
-			## If input geometry type is wkbUnknown, output features are not
-			## created, so we just set linestring, but it doesn't really matter
-			geom_type = in_layer.GetGeomType() or ogr.wkbLineString
-			out_layer = out_ds.CreateLayer(in_layer.GetName(), srs=LAMBERT1972,
-												geom_type=geom_type)
+		## Create backup first
+		backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
+		print("Copying backup file %s" % backup_filespec)
+		in_ds.Destroy()
+		backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
+		in_ds = ogr.Open(backup_filespec, 0)
+		in_layer = in_ds.GetLayer(0)
 
-			## Copy input Layer Fields to the output Layer
-			in_layer_defn = in_layer.GetLayerDefn()
-			for i in range(in_layer_defn.GetFieldCount()):
-				field_defn = in_layer_defn.GetFieldDefn(i)
-				out_layer.CreateField(field_defn)
+		## Create the output tab file
+		driver = ogr.GetDriverByName("MapInfo File")
+		out_ds = driver.CreateDataSource(mi_filespec)
 
-			## Get the output Layer's Feature Definition
-			out_layer_defn = out_layer.GetLayerDefn()
+		## Create the output Layer
+		## If input geometry type is wkbUnknown, output features are not
+		## created, so we just set linestring, but it doesn't really matter
+		geom_type = in_layer.GetGeomType() or ogr.wkbLineString
+		out_layer = out_ds.CreateLayer(in_layer.GetName(), srs=out_srs,
+											geom_type=geom_type)
 
-			## Add features to the ouput Layer
-			for in_feature in in_layer:
-				## Create output Feature
-				out_feature = ogr.Feature(out_layer_defn)
+		## Copy input Layer Fields to the output Layer
+		in_layer_defn = in_layer.GetLayerDefn()
+		for i in range(in_layer_defn.GetFieldCount()):
+			field_defn = in_layer_defn.GetFieldDefn(i)
+			out_layer.CreateField(field_defn)
 
+		## Get the output Layer's Feature Definition
+		out_layer_defn = out_layer.GetLayerDefn()
+
+		## Add features to the ouput Layer
+		for in_feature in in_layer:
+			## Create output Feature
+			out_feature = ogr.Feature(out_layer_defn)
+
+			## Copy feature from input layer
+			## (works in PY2, but messes up encoding in PY3)
+			if PY2:
+				out_feature.SetFrom(in_feature)
+
+			else:
 				## Add field values from input Layer
 				for i in range(out_layer_defn.GetFieldCount()):
-					field_defn = out_layer_defn.GetFieldDefn(i)
-					out_feature.SetField(out_layer_defn.GetFieldDefn(i).GetNameRef(),
-												in_feature.GetField(i))
+					value = in_feature.GetField(i)
+					if encoding and isinstance(value, type(u'')):
+						value = value.encode(encoding)
+						out_feature.SetFieldString(i, value)
+					else:
+						out_feature.SetField(i, value)
 
 				## Set geometry if present
 				geom = in_feature.GetGeometryRef()
 				if geom:
+					geom = geom.Clone()
+					#geom.AssignSpatialReference(out_srs)
 					#wkt = geom.ExportToWkt()
-					out_feature.SetGeometry(geom.Clone())
+					out_feature.SetGeometry(geom)
 					#out_geom = ogr.CreateGeometryFromWkt(wkt)
 					#out_feature.SetGeometry(out_geom)
 
-				## Add new feature to output Layer
-				errcode = out_layer.CreateFeature(out_feature)
-				#print(errcode)
+			## Add new feature to output Layer
+			errcode = out_layer.CreateFeature(out_feature)
+			#print(errcode)
 
-				## Destroy the feature to free resources
-				out_feature.Destroy()
+			## Destroy the feature to free resources
+			out_feature.Destroy()
 
-			out_layer.SyncToDisk()
+		out_layer.SyncToDisk()
 
-			# Close DataSources
-			in_ds.Destroy()
-			out_ds.Destroy()
+		# Close DataSources
+		in_ds.Destroy()
+		out_ds.Destroy()
 
-		else:
+		#else:
 			## Longitude / Latitude (Belgium): does not seem to be problematic
-			print("Warning: %s has older MapInfo implementation of Lambert1972, not fixed!"
-					% mi_filespec)
+			#print("Warning: %s has older MapInfo implementation of Lambert1972, not fixed!"
+			#		% mi_filespec)
 
 
 
-def fix_mi_lambert(mi_filespec, overwrite_backup=False):
+def fix_mi_lambert_mi(mi_filespec, overwrite_backup=False, dry_run=False):
 	import mapping.MIPython as MI
 
 	app = MI.Application()
@@ -143,36 +193,77 @@ def fix_mi_lambert(mi_filespec, overwrite_backup=False):
 	except:
 		tab.Close()
 	else:
-		new_lambert_clause = new_lambert_clauses[idx]
-		print("Fixing %s... with MIPython" % mi_filespec)
+		print('%s: detected older MapInfo implementation of Lambert1972'
+				% os.path.split(mi_filespec)[-1])
 
-		## Create backup first
-		tab.Close()
-		folder, filename = os.path.split(mi_filespec)
-		basename, ext = os.path.splitext(filename)
-		backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
-		print("Copying backup file %s" % backup_filespec)
-		backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
-		tab = app.OpenTable(backup_filespec)
-		tab.Backup(mi_filespec)
-		tab.Close()
+		if not dry_run:
+			new_lambert_clause = new_lambert_clauses[idx]
+			print("Fixing projection with MIPython")
 
-		## Fix coordinate system
-		tab = app.OpenTable(mi_filespec)
-		coordsys.FromClause(new_lambert_clause)
-		if bounds:
-			coordsys.SetBounds(bounds)
-		tab.SetCoordsys(coordsys, backup=False)
-		tab.Close()
+			## Create backup first
+			tab.Close()
+			folder, filename = os.path.split(mi_filespec)
+			basename, ext = os.path.splitext(filename)
+			backup_filespec = os.path.join(folder, basename + " (old lambert72)" + ext)
+			print("Copying backup file %s" % backup_filespec)
+			backup_mi_file(mi_filespec, backup_filespec, overwrite=overwrite_backup)
+			tab = app.OpenTable(backup_filespec)
+			tab.Backup(mi_filespec)
+			tab.Close()
+
+			## Fix coordinate system
+			tab = app.OpenTable(mi_filespec)
+			coordsys.FromClause(new_lambert_clause)
+			if bounds:
+				coordsys.SetBounds(bounds)
+			tab.SetCoordsys(coordsys, backup=False)
+			tab.Close()
 
 
-def fix_mi_lambert_folder_ogr(folder, overwrite_backup=False):
-	for dirpath, dirnames, filenames in os.walk(folder):
+def fix_mi_lambert_folder(folder, method='ogr', overwrite_backup=False,
+								encoding='latin-1', dry_run=False, recursive=False):
+	"""
+	Fix Belgian Lambert 1972 projection for all MapInfo files in a given folder
+
+	:param folder:
+		str, full path to folder containing MapInfo TAB files
+	:param method:
+		str, which method to use: either 'ogr' or 'mi'
+		(default: 'ogr')
+	:param overwrite_backup:
+		bool, whether or not to overwrite previous backup files (with appendix
+		'(old lambert72)')
+		(default: False)
+	:param encoding:
+		str, character encoding, required for PY3, which messes up the encoding
+		(default: 'latin-1')
+	:param dry_run:
+		bool, whether or not to keep files untouched, only reporting which ones
+		have a faulty projection
+		(default: False)
+	:param recursive:
+		bool, whether or not to include subfolders
+		(default: False)
+	"""
+	if recursive:
+		list_func = os.walk
+	else:
+		list_func = os.listdir
+
+	for (dirpath, dirnames, filenames) in os.walk(folder):
 		for filename in filenames:
 			if (os.path.splitext(filename)[-1].upper() == ".TAB"
 				and not "old lambert72" in filename):
 				mi_filespec = os.path.join(dirpath, filename)
-				fix_mi_lambert_ogr(mi_filespec, overwrite_backup=overwrite_backup)
+				if method == 'ogr':
+					fix_mi_lambert_ogr(mi_filespec, overwrite_backup=overwrite_backup,
+										encoding=encoding, dry_run=dry_run)
+				elif method == 'mi':
+					fix_mi_lambert_mi(mi_filespec, overwrite_backup=overwrite_backup,
+										dry_run=dry_run)
+
+		if not recursive:
+			break
 
 
 
